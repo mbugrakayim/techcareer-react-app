338ae439ea195b89d00f4c8e9af53a49cc394215
Mustafa Buğra Kayim <mbugrakayim@gmail.com>
2023-10-14 12:00:40 +0300

    ders ödev

---
+++ src/Components/PageContent/PageContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProjectSection from './ProjectSection'
+import AboutSection from './AboutSection'
+import ContactSection from './ContactSection'
+
+function PageContent() {
+    return (
+        <div className="w3-content w3-padding" style={{ maxWidth: 1564 }}>
+
+            <ProjectSection />
+            <AboutSection />
+            <ContactSection />
+        </div>
+
+    )
+}
+
+export default PageContent
